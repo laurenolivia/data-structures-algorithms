7795cf595628478911afbcdf1564ba6140c92469
Lauren Burwell <laurenburwell@Laurens-MacBook-Pro.local>
2018-10-02 16:24:28 -0700

    completed binary search example, needs comments

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,19 @@
-def binary_search(list, item):
-	
+def binary_search(alist, item):
+
+	if len(alist) == 0:
+		return False
+	else:
+		midpoint = len(alist)//2
+		if alist[midpoint] == item:
+			return True
+		else:
+			if item < alist[midpoint]:
+				return binary_search(alist[:midpoint-1], item)
+			else:
+				return binary_search(alist[midpoint+1:], item)			
+
+
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+print(binary_search(testlist, 5))
+print(binary_search(testlist, 23))
