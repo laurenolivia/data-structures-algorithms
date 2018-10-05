8e94b40ebf259862bb19daeb6d6c386294259ae7
Lauren Burwell <laurenburwell@Laurens-MacBook-Pro.local>
2018-10-04 18:57:38 -0700

    refactored

--- binary_search.py
+++ binary_search.py
@@ -8,12 +8,12 @@ def binary_search(alist, item):
 			return True
 		else:
 			if item < alist[midpoint]:
-				return binary_search(alist[:midpoint-1], item)
+				return binary_search(alist[:midpoint], item)
 			else:
 				return binary_search(alist[midpoint+1:], item)			
 
 
 
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
-print(binary_search(testlist, 5))
+print(binary_search(testlist, 6))
 print(binary_search(testlist, 23))
